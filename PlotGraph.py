5c497d0e0c27dc11aa23bc02c91a222d7414d73b
Chen <czha959@aucklanduni.ac.nz>
2019-07-01 21:44:41 +1200

    Code Cleaning

--- PlotGraph.py
+++ PlotGraph.py
@@ -4,10 +4,12 @@ from os.path import isfile, join
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# change path to the location of Records folder when you use this on your device
+csvFolderPath = 'E:/Part4Project/PreviousProject/vr-earthquake-lab/VR Earthquake Lab/Records'
+path = Path(csvFolderPath).absolute()
+
 
 def readFile(filename):
-    # change path to the location of Records folder when you use this on your device
-    path = Path('E:/Part4Project/PreviousProject/vr-earthquake-lab/VR Earthquake Lab/Records').absolute()
     dp = path.joinpath(filename)
     df = pd.read_csv(dp)
     return df
@@ -18,15 +20,14 @@ def plotGraph(df, filename):
     plt.title('displacement vs time')
     plt.ylabel('object displacement')
     plt.xlabel('time')
-    # Need to give a name for each of the graph when it saves multiple images.
+
     sep = '.'
     name = filename.split(sep, 1)[0]
-    plt.savefig('./images/' + name + '.png')
+    plt.savefig('./images/' + name + '.png')  # generated plots will be saved inside the images folder
     return
 
 
 def main():
-    path = Path('E:/Part4Project/PreviousProject/vr-earthquake-lab/VR Earthquake Lab/Records').absolute()
     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
     for file in onlyfiles:
         df = readFile(file)
