ed57e3d43205191ae1a404b6808d76223785d288
Chen <czha959@aucklanduni.ac.nz>
2019-07-04 20:28:00 +1200

    modified the plot to show 3 rotation graph side by side

--- PlotGraph.py
+++ PlotGraph.py
@@ -12,18 +12,40 @@ path = Path(csvFolderPath).absolute()
 def readFile(filename):
     dp = path.joinpath(filename)
     df = pd.read_csv(dp)
-    return df
+    df1 = df.iloc[20:]
+    print(df1.head())
+    return df1
 
 
 def plotGraph(df, filename):
-    plt.plot(df[df.columns[0]], df[df.columns[1]], 'g--')
-    plt.title('displacement vs time')
-    plt.ylabel('object displacement')
+    x = df[df.columns[0]]
+    y0 = df[df.columns[1]]
+    y1 = df[df.columns[2]]
+    y2 = df[df.columns[3]]
+
+    plt.figure(figsize=(20, 4))
+    plt.subplot(1, 3, 1)
+    plt.plot(x, y0, 'g--')
+    plt.title('x-orientation vs time')
+    plt.ylabel('object orientation')
     plt.xlabel('time')
 
+    plt.subplot(1, 3, 2)
+    plt.plot(x, y1, 'b--')
+    plt.title('y-orientation vs time')
+    plt.ylabel('object orientation')
+    plt.xlabel('time')
+
+    plt.subplot(1, 3, 3)
+    plt.plot(x, y2, 'r--')
+    plt.title('z-orientation vs time')
+    plt.ylabel('object orientation')
+    plt.xlabel('time')
+    plt.tight_layout()
     sep = '.'
     name = filename.split(sep, 1)[0]
     plt.savefig('./images/' + name + '.png')  # generated plots will be saved inside the images folder
+    # plt.show()
     plt.clf()
     plt.cla()
     plt.close()
