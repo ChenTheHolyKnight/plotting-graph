74025cb564d9d0943da64e49da930a10cea2531c
Chen <czha959@aucklanduni.ac.nz>
2019-09-11 19:19:03 +1200

    Being able to plot the three groups in a better layout

--- NormalityTest.py
+++ NormalityTest.py
@@ -1,6 +1,8 @@
 from os import listdir
 from os.path import isfile, join
 from pathlib import Path
+import seaborn as sns
+import numpy as np
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -65,6 +67,34 @@ def process_whit(df1, df2):
     do_man_whitney_u_test(lista3, listb3)
 
 
+def draw_box_plot(list1, list2):
+    ticks = ['Quiz 1', 'Quiz 2', 'Quiz 3']
+
+    def set_box_color(bp, color):
+        plt.setp(bp['boxes'], color=color)
+        plt.setp(bp['whiskers'], color=color)
+        plt.setp(bp['caps'], color=color)
+        plt.setp(bp['medians'], color="#008000")
+
+    plt.figure()
+
+    bpl = plt.boxplot(list1, positions=np.array(range(len(list1))) * 2.0 - 0.4, sym='', widths=0.6)
+    bpr = plt.boxplot(list2, positions=np.array(range(len(list2))) * 2.0 + 0.4, sym='', widths=0.6)
+    set_box_color(bpl, '#D7191C')  # colors are from http://colorbrewer2.org/
+    set_box_color(bpr, '#2C7BB6')
+
+    # draw temporary red and blue lines and use them to create a legend
+    plt.plot([], c='#D7191C', label='Group A (VR First)')
+    plt.plot([], c='#2C7BB6', label='Group B (Reading First)')
+    plt.legend()
+
+    plt.xticks(range(0, len(ticks) * 2, 2), ticks)
+    plt.xlim(-2, len(ticks) * 2)
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig('./' + '/VR_Quiz_Box_Plot' + '.svg')
+
+
 def main():
     # normality_test()
 
@@ -81,10 +111,17 @@ def main():
     df_total['Reading_2'] = df2['Qustionnaire2']
     df_total['Reading_3'] = df2['Qustionnaire3']
 
-    df_total.boxplot(
-        column=['VR_1', 'Reading_1', 'VR_2', 'Reading_2'])
+    list1 = [df1['Qustionnaire1'].values.tolist(), df1['Qustionnaire2'].values.tolist(),
+             df1['Qustionnaire3'].values.tolist()]
+    list2 = [df2['Qustionnaire1'].values.tolist(), df2['Qustionnaire2'].values.tolist(),
+             df2['Qustionnaire3'].values.tolist()]
+    draw_box_plot(list1, list2)
+
+    # df_total.boxplot(
+    #     column=['VR_1', 'Reading_1', 'VR_2', 'Reading_2', 'VR_3', 'Reading_3'])
+    #
     # plt.show()
-    plt.savefig('./' + '/VR_Reading_Box_Plot' + '.png')
+    # plt.savefig('./' + '/VR_Reading_Box_Plot' + '.png')
     return
 
 
